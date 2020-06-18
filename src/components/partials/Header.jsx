352b2927bae6e31679f3356e0320ec7ad12df2c3
shakyasaijal <saijalshakya@gmail.com>
2020-06-18 22:28:04 +0545

    ref: unwanted code

--- src/components/partials/Header.jsx
+++ src/components/partials/Header.jsx
@@ -112,18 +112,9 @@ const Header = (props) => {
       </header>
       <MobileNavigation />
       {isLoggedIn && !isAccountVerified && (
-        <div className="warning-wrapper">
-          <div className="warning">
-            Please verify your account.
-            <div className="float-right">
-              <Link to="/" className="warning-button">
-                Resend Verification Link
-              </Link>
-            </div>
-          </div>
-        </div>
+        <NotVerifiedModal />
       )}
-      <NotVerifiedModal/>
+
     </>
   );
 };
