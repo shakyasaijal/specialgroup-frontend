05b7303a76a6600aa65755d18487e5a0681e8b4c
shakyasaijal <saijalshakya@gmail.com>
2020-06-20 22:25:44 +0545

    bug: logout btn

--- src/components/partials/Header.jsx
+++ src/components/partials/Header.jsx
@@ -100,6 +100,7 @@ const Header = (props) => {
                 </div>
                 <div className="info grid-template grid-row-account float-right paddingLeft-5">
                   {account.firstName && <small className="anchor">Welcome, {account.firstName}</small>}
+                  {!isLoggedIn && <small className="anchor">Welcome</small>}
                   {!isLoggedIn && <Link to={PATHS.SIGNIN}>LogIn</Link>}
                   <span className="my-account anchor">
                     {isLoggedIn && (
