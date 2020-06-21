65172374c2e8625ce8e54a556e1b0eef86e17969
shakyasaijal <saijalshakya@gmail.com>
2020-06-21 22:40:19 +0545

    feat: ads managed

--- src/components/Advertisement/FullWidthAd.jsx
+++ src/components/Advertisement/FullWidthAd.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 
-const FullWidthAd = () => {
+const FullWidthAd = (props) => {
+  let image = props.large
+  let hasAd = false;
+  console.log(window.innerWidth)
+  if (props.large || props.small) {
+    hasAd = true;
+  }
+  if (window.innerWidth <= 492) {
+    image = props.small;
+  }
   return (
     <div className="row">
-      <div className="panel ad">
-        <div className="container verticle-center text-center">
-          <h4 className="medium-dark"> Ad Space Available Here * </h4>
-          <span className="subtitle">Contact Us: +977 - 98xxxxxxx</span>
-        </div>
+      <div className="advertisement">
+        {
+          hasAd ? <div className="ad-container">
+            <img src={image} />
+          </div> : <div className="panel ad">
+              <div className="container verticle-center text-center">
+                <h4 className="medium-dark"> Ad Space Available Here * </h4>
+                <span className="subtitle">Contact Us: +977 - 98xxxxxxx</span>
+              </div>
+            </div>
+        }
       </div>
     </div>
   );
