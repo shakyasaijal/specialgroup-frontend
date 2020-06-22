a09cddd5efb54dcf6d825c74cf210f5b3826bb62
Ram Bhandari <rambhandari51@gmail.com>
2020-06-22 10:40:04 +0545

    IMplement Resend verification mail api

--- src/components/partials/NotVerifiedModal.jsx
+++ src/components/partials/NotVerifiedModal.jsx
@@ -1,8 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
 import Backdrop from '@material-ui/core/Backdrop';
 import Fade from '@material-ui/core/Fade';
+
+import { resendVerificationEmailRequest } from 'actions/auth';
+
+import { isAccountVerified } from 'selectors/auth';
 
 const useStyles = makeStyles((theme) => ({
   modal: {
@@ -22,10 +28,12 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const NotVerifiedModal = () => {
-  const verified = false;
+const NotVerifiedModal = (props) => {
+  const isVerified = props.isVerified;
   const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
+  const [emailSent, setEmailSent] = useState(false);
+  const [response, setResponse] = useState('Sorry, Email has not been sent. Please check your email and try again');
 
   const handleOpen = () => {
     setOpen(true);
@@ -36,15 +44,59 @@ const NotVerifiedModal = () => {
   };
 
   useEffect(() => {
-    if (!verified) {
+    if (!isVerified) {
       handleOpen();
     }
-  }, [verified]);
+  }, [isVerified]);
 
   const handleClick = () => {
-    console.log('Clicked');
-    handleClose();
+    const { email, resendVerificationEmailRequest } = props;
+    resendVerificationEmailRequest(email, callbackSuccess);
   };
+
+  const callbackSuccess = () => {
+    setEmailSent(true);
+    setResponse(`We have sent you an email at ${props.email} Please check your email inbox.`);
+  };
+
+  const resendMailContent = (
+    <div className={classes.paper}>
+      <div className="container center text-center">
+        <div className="description">
+          <div className="reason center">
+            You have <b>not verified</b> your account yet. If you have missed it, please press the button below.
+          </div>
+          <div className="btn">
+            <button onClick={handleClick}>Resend Verification Link</button>
+          </div>
+        </div>
+      </div>
+      <div className="later">
+        <div className="float-right">
+          <span onClick={handleClose} className="maybe-later">
+            Maybe Later
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+
+  const mailSentContent = (
+    <div className={classes.paper}>
+      <div className="container center text-center">
+        <div className="description">
+          <div className="reason center">{response}</div>
+        </div>
+      </div>
+      <div className="later">
+        <div className="float-right">
+          <span onClick={handleClose} className="maybe-later">
+            Close
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 
   return (
     <Modal
@@ -60,30 +112,18 @@ const NotVerifiedModal = () => {
       }}
     >
       <div className="not-verified">
-        <Fade in={open}>
-          <div className={classes.paper}>
-            <div className="container center text-center">
-              <div className="description">
-                <div className="reason center">
-                  You have <b>not verified</b> your account yet. If you have missed it, please press the button below.
-                </div>
-                <div className="btn">
-                  <button onClick={handleClick}>Resend Verification Link</button>
-                </div>
-              </div>
-            </div>
-            <div className="later">
-              <div className="float-right">
-                <span onClick={handleClose} className="maybe-later">
-                  Maybe Later
-                </span>
-              </div>
-            </div>
-          </div>
-        </Fade>
+        <Fade in={open}>{!emailSent ? resendMailContent : mailSentContent}</Fade>
       </div>
     </Modal>
   );
 };
 
-export default NotVerifiedModal;
+const mapStateToProps = (state) => {
+  return {
+    email: state.account.email || '',
+    isVerified: isAccountVerified(state),
+  };
+};
+
+const dispatchProps = { resendVerificationEmailRequest };
+export default connect(mapStateToProps, dispatchProps)(NotVerifiedModal);
