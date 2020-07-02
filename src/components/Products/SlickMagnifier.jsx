ccb4604e0f68381e722d9d135dcfc7f510cfa98c
shakyasaijal <saijalshakya@gmail.com>
2020-07-02 19:32:50 +0545

    filters

--- src/components/Products/SlickMagnifier.jsx
+++ src/components/Products/SlickMagnifier.jsx
@@ -3,25 +3,29 @@ import React from 'react';
 // import Slick from 'react-slick';
 
 const SlickMagnifier = () => {
-  //   const allImages = props.product.images;
-  //   const dataSource = [];
+  // const allImages = props.product.images;
+  // const dataSource = [];
 
-  //   for (let i = 0; i < allImages.length; i++) {
-  //     dataSource.push({
-  //       small: allImages[i],
-  //       large: allImages[i],
-  //     });
-  //   }
+  // for (let i = 0; i < allImages.length; i++) {
+  //   dataSource.push({
+  //     small: allImages[i],
+  //     large: allImages[i],
+  //   });
+  // }
 
-  //   const rimProps = { enlargedImagePortalId: 'portal' };
-  //   const rsProps = {
-  //     enlargedImageContainerDimensions: {
-  //       width: '',
-  //       height: '',
-  //     },
-  //   };
+  // const rimProps = { enlargedImagePortalId: 'portal' };
+  // const rsProps = {
+  //   enlargedImageContainerDimensions: {
+  //     width: '',
+  //     height: '',
+  //   },
+  // };
 
-  return <></>;
+  return (
+    <>
+      {/* <img src="http://media.gucci.com/style/DarkGray_Center_0_0_490x490/1590421508/630715_XJBUW_3806_001_100_0000_Light-Cotton-jersey-shorts.jpg" alt="asd"/> */}
+    </>
+  );
 };
 
 export default SlickMagnifier;
